c6304a27fc14be286ed725488886c6f78fa7457d
Syed Rizwan Ahmed <rizwan.zip@gmail.com>
2023-08-07 21:13:32 +0500

    Initial commit

---
+++ src/main/java/com/example/jenkinsgradle/JenkinsGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinsgradle/JenkinsGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
